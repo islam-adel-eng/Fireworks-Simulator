07a42eecc986a79ae5a79e5226c7c466351563dd
Islam Adel <Islam.adel.engineer@gmail.com>
2025-09-12 17:27:30 +0400

    Initial commit
    base SDL3

---
+++ main.c
@@ -0,0 +1,34 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdio.h>
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+    if (!SDL_Init)
+    {
+        return SDL_APP_FAILURE;
+    }
+    
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    
+}
